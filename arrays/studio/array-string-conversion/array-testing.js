1ca8ab31eb782693e32dbee2bb4747ad71246b69
treyhickman10 <thickman10@gmail.com>
2024-06-20 20:26:58 -0500

    Added partial code from studio

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -8,21 +8,26 @@ strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
+	let check = strings[0];
+	let output = [];
 	//TODO: 2. write the code required for this step
+
+	output = check.split(",").reverse().join(",");
+
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
-
+console.log(reverseCommas());
+console.log(typeof output)
 //3)
 function semiDash() {
-	let check;
-	let output;
+	let check = strings[1];
+	let output = [];
 //TODO: write the code required for this step
-
-  
+	
+	output = check.split(";").reverse().join(";");
+	
 	return output;
 }
 
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,38 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
+
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+let start = str.slice(0, 3);
+let final = str.slice(3, str.length);
+let word = final + start;
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+console.log(`The mutated version of ${str} is ${word}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+let number = input.question(`Insert a number between 1 and ${str.length}: `);
+
+let start1 = str.slice(0, number);
+let final1 = str.slice(number, str.length);
+let word1 = final1 + start1;
+
+
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+if (number > 0 && number < 10) {
+    let start1 = str.slice(0, number);
+    let final1 = str.slice(number, str.length);
+    let word1 = final1 + start1;
+    console.log(`The mutated version of ${str} is ${word1}`);  
+}  else {
+    let start = str.slice(0, 3);
+    let final = str.slice(3, str.length);
+    let word = final + start;
+    console.log(`The mutated version of ${str} is ${word}. We defaulted to index of 3 because your number was outside of the accepted range.`);
+}
